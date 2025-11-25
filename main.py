44a874f8e86bb6586058844c54c683a7e0733b25
hancymahajan84 <hancymahajan84@gmail.com>
2025-11-25 15:34:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import streamlit as st
 st.title('learning streamlit programing')
 a=st.text_input('enter your name')
 mm=st.number_input('enter your ip marks')
-st.write(f'hello! {a} , your ip marks is {mm}')
+if st.button('Submit'):
+  st.write(f'hello! {a} , your ip marks is {mm}')
+
